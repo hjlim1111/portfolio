01c7964683b3e72fec61acc83bdc0704263c1b20
hylim <gywjd8938@naver.com>
2022-09-22 16:48:22 +0900

    upload

--- html/shilla/js/script.js
+++ html/shilla/js/script.js
@@ -14,29 +14,7 @@ $(document).ready(function(){
     });*/
 
     /* nav */
-    $('.btn_nav').click(function(e){
-        e.preventDefault();
-        $('header').toggleClass('active');
-        $('#nav_wrap').slideToggle();
-    });
-    
-    $(window).resize(function(){
-        if($(this).width() >= 1300){
-            $('header').removeClass('active');
-            $('#nav_wrap').show();
-        } else{
-            $('#nav_wrap').hide();
-        }
-    });
 
-
-
-    /*submenu 나타나게*/
-    $("ul.gnb>li").hover(function ( ) {
-        $(".nav_bg,.submenu").stop().slideDown();
-    },function ( ) {
-        $(".nav_bg,.submenu").stop().slideUp();
-    });
 
 
   /*

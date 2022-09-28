620cf65dab3e78c2bda34b99e0bde23e7058ce41
hylim <gywjd8938@naver.com>
2022-09-28 10:57:44 +0900

    upload

--- html/shilla/js/header.js
+++ html/shilla/js/header.js
@@ -18,12 +18,20 @@ $(document).ready(function(){
     
     /*submenu 나타나게*/
     $('ul.gnb > li').hover(function ( ) {
-        if(window.innerWidth >= 1300){
+        if(window.innerWidth > 1300){
             $('.navBg,.submenu').stop().slideDown();
-        }
+        } 
      },function ( ) {
-        if(window.innerWidth >= 1300){
+        if(window.innerWidth > 1300){
             $('.navBg,.submenu').stop().slideUp();
         }
+     });
+
+     /* 1300이하일 때 서브메뉴 각각 나타나게 표현 */
+     $('ul.gnb > li').click(function ( ) {
+        if(window.innerWidth <= 1300){
+            $(this).stop().slideToggle();
+            console.log(this);
+        } 
      });
 });
